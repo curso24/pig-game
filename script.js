499544fca9d0762e306515ac23e771721d09fcac
curso24 <tripleene333@gmail.com>
2024-01-26 13:20:30 +0100

    añadimos botones de btn

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+//seleccionar elementos del DOM
 
+const btnNew = document.querySelector('.btn--new')
+const btnRoll = document.querySelector('.btn--roll')
+const btnHold = document.getElementsByClassName('btn--hold')[0]
+
+console.log(btnNew, btnRoll, btnHold)
+
+btnRoll.addEventListener('click', () => {
+  console.log('Roll dice')
+})
+
+btnNew.addEventListener('click', () => {
+  console.log('New Game')
+})
+
+btnHold.addEventListener('click', () => {
+  console.log('Pasar turno')
+})
