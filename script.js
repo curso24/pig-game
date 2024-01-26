fee3373c4777c13a35b145a80c5c530ffdadd1bc
curso24 <tripleene333@gmail.com>
2024-01-26 14:08:49 +0100

    llamar a función init que haga lo siguiente:  // el dado por defecto es invisible // score1 y score2 a 0 // currentScore1 y currentScore2 a 0

--- script.js
+++ script.js
@@ -2,8 +2,30 @@ const btnNew = document.querySelector('.btn--new')
 const btnRoll = document.querySelector('.btn--roll')
 const btnHold = document.querySelector('.btn--hold')
 const diceEl = document.querySelector('.dice')
+const score0El = document.querySelector('#score--0')
+const score1El = document.querySelector('#score--1')
+const current0El = document.querySelector('#current--0')
+const current1El = document.querySelector('#current--1')
 
 // const btnHold = document.getElementsByClassName('btn--hold')[0]
+
+// TODO:
+// llamar a función init que haga lo siguiente:
+
+// el dado por defecto es invisible
+// score1 y score2 a 0
+// currentScore1 y currentScore2 a 0
+
+const init = function () {
+  // diceEl.clasList.add('hidden')
+  diceEl.style.display = 'none'
+  score0El.textContent = 0
+  score1El.textContent = 0
+  current0El.textContent = 0
+  current1El.textContent = 0
+}
+
+init()
 
 console.log(btnNew, btnRoll, btnHold)
 
@@ -12,6 +34,7 @@ btnRoll.addEventListener('click', () => {
   const dice = Math.trunc(Math.random() * 6) + 1
 
   // mostrar el dado
+  diceEl.style.display = 'block'
   diceEl.src = `dice-${dice}.png`
 
   if (dice === 1) {
@@ -30,13 +53,5 @@ btnHold.addEventListener('click', () => {
 })
 
 // TODO:
-// llamar a función init que haga lo siguiente:
-// el dado por defecto es invisible
-// score1 y score2 a 0
-// currentScore1 y currentScore2 a 0
-
-// TODO:
 // cuando se hace click en el botón roll:
 // el dado se pone visible
-
-
